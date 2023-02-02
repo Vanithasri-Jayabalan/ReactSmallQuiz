a5d4a0f7f6f62656a1e5c40ac8ec5fdd68439452
Vanithasri Jayabalan <Vanithasri.Jayabalan@cudirect.com>
2023-02-02 05:58:23 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import questions from './questions';
+import { useState } from 'react';
 
 function App() {
+ const [questionIndex, setQuestionIndex] = useState(0);
+ const [score, setScore] = useState(0);
+ const [showScore, setShowScore] = useState(false);
+ const [value, setValue] = useState(500);
+
+ const currentQuestion = questions[questionIndex];
+
+ const selectOption = (idx) => {
+  if(currentQuestion.answer === idx)
+  {
+    setScore(score + 1);
+  }
+  const nextQ = questionIndex + 1;
+  if (nextQ < questions.length) {
+    setQuestionIndex(questionIndex + 1);
+  }else{
+    setShowScore(true);
+  }
+ };
+const reset = () => {
+  setQuestionIndex(0);
+  setScore(0);
+  setShowScore(false);
+};
+const increase = () => {
+  setValue(value + 1);
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz_container">
+      { showScore ? 
+      (<div>
+      <h1>Quiz Completed</h1>
+      <h1>Your Score is: {score}</h1><br/>
+      <button className='button' onClick={reset}> Start New </button>
+      </div>) : (<div className='quiz_question'>
+       <p>{currentQuestion.question}</p>
+      <div className='quiz_answer'>
+        <ul className='quiz_answerul'>
+          {currentQuestion.options.map((option, i) => {
+          return <li className='quiz_answerli' onClick={() => selectOption(i)}>{option}</li>
+          })}
+          </ul>
+      </div>
+      </div>)}
+      {/* <button onClick={increase}>Increase Value: {value}</button> */}
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default[
+    {
+    question: "What does HTML stand for?",
+    options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyperlinks and Text Makeup Language"],
+    answer: 1,
+},
+{
+    question: "Who is making the Web standards?",
+    options: ["Mozilla", "Google", "Microsoft", "The World Wide Web Consortium"],
+    answer: 2,
+},
+{
+    question: "Choose the correct HTML element for the largest heading?",
+    options: ["<heading>", "<h6>", "<head>", "<h1>"],
+    answer: 0,
+},
+{
+    question: "What is the correct HTML element for inserting a line break?",
+    options: ["<break>", "<br>", "<lb>", "<hr>"],
+    answer: 1,
+}
+];
